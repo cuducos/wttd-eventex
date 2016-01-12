4bd59c5ed695f8e078696a1de3289ab01886c2bd
Eduardo Cuducos <cuducos@gmail.com>
2016-01-12 10:03:02 -0200

    Adiciona Inscrições no /admin

---
+++ eventex/subscriptions/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.contrib import admin
+
+
+class SubscriptionsConfig(AppConfig):
+    name = 'eventex.subscriptions'
+    verbose_name = 'Controle de Inscrições'
