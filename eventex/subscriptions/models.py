9a5d94528416ad3f80c36d81416132a85337d15c
Eduardo Cuducos <cuducos@gmail.com>
2016-01-19 15:33:27 -0200

    Adiciona campo pago

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -8,6 +8,7 @@ class Subscription(models.Model):
     email = models.EmailField('e-mail')
     phone = models.CharField('telefone', max_length=20)
     created_at = models.DateTimeField('criado em', auto_now_add=True)
+    paid = models.BooleanField('pagamento', default=False)
 
     def __str__(self):
         return self.name
