7ddcabbaec91726066c8eba1ef2c3daf717a5df0
Eduardo Cuducos <cuducos@gmail.com>
2016-01-26 16:32:32 -0200

    Adiciona palestrantes

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -18,3 +18,15 @@ class HomeTest(TestCase):
     def test_subscription_link(self):
         expected = 'href="{}"'.format(resolve_url('subscriptions:new'))
         self.assertContains(self.response, expected)
+
+    def test_speakers(self):
+        """Must show keynote speakers"""
+        contents = ['Grace Hopper', 'http://hbn.link/hopper-pic',
+                    'Alan Turing', 'http://hbn.link/turing-pic']
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+    def test_speakers_link(self):
+        expected = 'href="{}#speakers"'.format(resolve_url('home'))
+        self.assertContains(self.response, expected)
